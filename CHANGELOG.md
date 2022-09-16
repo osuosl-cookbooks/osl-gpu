ddd63566286ca17a6c8360bfefccab3412181801
Manatee <github@osuosl.org>
2022-09-16 23:36:59 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the osl-gpu cookbook.
 
+1.0.0 (2022-09-16)
+------------------
+- Initial resources
+
 ## 0.1.0
 
 - Initial release
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs/Configures osl-gpu'
 issues_url        'https://github.com/osuosl-cookbooks/osl-gpu/issues'
 source_url        'https://github.com/osuosl-cookbooks/osl-gpu'
 chef_version      '>= 16.0'
-version           '0.1.0'
+version           '1.0.0'
 
 supports          'centos', '~> 7.0'
 supports          'centos_stream', '~> 8.0'

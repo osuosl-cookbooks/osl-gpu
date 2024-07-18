38d0cb24a8e1074c2a122599e48238818c14637c
Manatee <github@osuosl.org>
2024-07-18 22:17:49 +0000

    Automatic minor-level version bump to v1.2.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the osl-gpu cookbook.
 
+1.2.0 (2024-07-18)
+------------------
+- Remove support for CentOS 7 and CentOS Stream 8
+
 1.1.0 (2023-03-15)
 ------------------
 - Add AlmaLinux 8 support
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs/Configures osl-gpu'
 issues_url        'https://github.com/osuosl-cookbooks/osl-gpu/issues'
 source_url        'https://github.com/osuosl-cookbooks/osl-gpu'
 chef_version      '>= 16.0'
-version           '1.1.0'
+version           '1.2.0'
 
 supports          'almalinux', '~> 8.0'
 supports          'ubuntu', '~> 20.04'
